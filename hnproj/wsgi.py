5f2066ef0df4562212ef8a8fe494f740fcf164da
Jonathan Hernandez <jbhernan@gmail.com>
2014-11-16 19:03:06 -0800

    fixed reqs and wsgi

--- hnproj/wsgi.py
+++ hnproj/wsgi.py
@@ -1,16 +1,17 @@
 """
-WSGI config for gettingstarted project.
+WSGI config for hnproj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
