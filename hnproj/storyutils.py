25ff36e7b2d2be40244e7f813f4f13f725a3a84a
Jonathan Hernandez <jbhernan@gmail.com>
2019-05-05 11:24:57 -0700

    urls

--- hnproj/storyutils.py
+++ hnproj/storyutils.py
@@ -1,15 +1,15 @@
-import urllib2
+from urllib.request import urlopen
 import json
 
 def get_max_item_id():
     maxItemUrl = 'https://hacker-news.firebaseio.com/v0/maxitem.json'
-    maxItemData = json.load(urllib2.urlopen(maxItemUrl))
+    maxItemData = json.load(urlopen(maxItemUrl))
     return long(maxItemData);
 
 # requests the item with the given id, return the json object.
 def get_item(item_id):
     url = 'https://hacker-news.firebaseio.com/v0/item/' + str(item_id) + '.json'
-    return json.load(urllib2.urlopen(url))
+    return json.load(urlopen(url))
 
 def is_story(item_json):
     return "story" == item_json.get('type')
@@ -20,7 +20,7 @@ def is_deleted(item_json):
 # Returns the top items json.
 def get_top_items():
   url = 'https://hacker-news.firebaseio.com/v0/topstories.json';
-  data = json.load(urllib2.urlopen(url));
+  data = json.load(urlopen(url));
   return data
 
 
