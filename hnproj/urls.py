e0b918c0f07c967e0e63b85167f2cca5851eae25
Jonathan Hernandez <jbhernan@gmail.com>
2023-12-18 14:56:07 -0600

    urls fix

--- hnproj/urls.py
+++ hnproj/urls.py
@@ -8,13 +8,12 @@ import hnproj.topstoriesviews
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'hnproj.views.newsByUser', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     path('admin/', admin.site.urls),
     
-    path('/', hnproj.views.newsByUser),
-    path('', hnproj.views.newsByUser),
+    path('/', hnproj.topstoriesviews.home),
+    path('', hnproj.topstoriesviews.home),
     path('users', hnproj.views.users),
     path('cronAddCurrTopItems', hnproj.views.update_top_items),
     path('removeTopStories', hnproj.views.remove_top_items),
